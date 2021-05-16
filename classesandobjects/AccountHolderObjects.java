1c589d588efcd62ac3c60f7117824ae66d74e0da
apetr <apetr@10.100.134.253>
2021-05-16 13:01:57 +0300

    added one more object

--- classesandobjects/AccountHolderObjects.java
+++ classesandobjects/AccountHolderObjects.java
@@ -7,16 +7,23 @@ public class AccountHolderObjects {
 		AccountHolder henry=new AccountHolder();
 		tom.firstName="Tom";
 		tom.lastName="Smith";
-		tom.age=27;
+		tom.age=21;
 		tom.accountBalance=40000; 	
 		tom.testEligibilityforCreditCard();
 		System.out.println("Is Tom eligible for Credir Card: "+tom.EligibilityforCreditCard);
 		
-		sarah.firstName="Tom";
-		sarah.lastName="Smith";
+		sarah.firstName="sarah";
+		sarah.lastName="doe";
 		sarah.age=31;
 		sarah.accountBalance=30000; 	
 		sarah.testEligibilityforCreditCard();
+		System.out.println("Is Sarah eligible for Credir Card: "+sarah.EligibilityforCreditCard);
+		
+		henry.firstName="henry";
+		henry.lastName="bowden";
+		henry.age=31;
+		henry.accountBalance=19000; 	
+		henry.testEligibilityforCreditCard();
 		System.out.println("Is Sarah eligible for Credir Card: "+sarah.EligibilityforCreditCard);
 	}
 
